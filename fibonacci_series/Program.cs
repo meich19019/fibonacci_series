96df02fb2c0da7e9d23be17a5e175055faab2edd
梅智皓 <meich19019@gmail.com>
2022-05-27 15:35:50 +0800

    Initial commit

---
+++ fibonacci_series/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fibonacci_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FibonacciSeries(10));
+        }
+
+        public static int FibonacciSeries(int n)
+        {
+            int result = 1;
+            int lastResult = 1;
+            int lastLastResult = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                result = lastResult + lastLastResult;
+                lastLastResult = lastResult;
+                lastResult = result;
+            }
+
+            return result;
+        }
+    }
+}
